7f5af2c1c04db0da194ec9eb14d0d0cb92993bfa
Brian.Chu <chuchengpeng0119@163.com>
2019-01-15 18:41:21 +0800

    Update FlyweightPattern.swift

--- DesignPattern/ReadMe/FlyweightPattern.swift
+++ DesignPattern/ReadMe/FlyweightPattern.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 /*
- ** Flyweight Pattern(享元模式)
+ ** Flyweight Pattern(享元模式)，池技术的重要实现方式
+ ** Use sharing to support large numbers of fine-grained objects efficiently.
+ ** 使用共享对象可有效地支持大量的细粒度的对象
  */
 
 class FlyweightPattern: NSObject {
